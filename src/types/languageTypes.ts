4bf7599c02b13d28b4a74383e7e2efd270bf137a
Igor <iluzarraga@gmail.com>
2023-10-29 12:39:39 +0100

    Cleaning some texts

--- src/types/languageTypes.ts
+++ src/types/languageTypes.ts
@@ -1,15 +1,7 @@
-import { TestimonialsType } from "./testimonialsTypes";
-import { ProjectsType } from "./projectsTypes";
-
 export type TextsType = {
   navbarMenu: {
     home: string;
-    skills: string;
-    projects: string;
-    testimonials: string;
-    contact: string;
     login: string;
-    // logout: string;
   };
   user: {
     name: string;
@@ -21,42 +13,11 @@ export type TextsType = {
     requiredField: string;
   };
   landing: {
-    welcome: string;
-    heading: string;
-    connectText: string;
-    contactBtn: string;
-  };
-  mySkills: {
-    titlePart1: string;
-    titlePart2: string;
-    heading: string;
-    skill1Title: string;
-    skill1Desc: string;
-    skill2Title: string;
-    skill2Desc: string;
-    skill3Title: string;
-    skill3Desc: string;
-  };
-  projects: {
-    titlePart1: string;
-    titlePart2: string;
-    heading: string;
-    projectsArr: ProjectsType;
-  };
-  testimonials: {
-    titlePart1: string;
-    titlePart2: string;
-    heading: string;
-    testimonialArr: TestimonialsType;
-  };
-  contact: {
-    titlePart1: string;
-    titlePart2: string;
-    formPlaceholderName: string;
-    formPlaceholderEmail: string;
-    formPlaceholderMsg: string;
-    semdMsgBtn: string;
-  };
+    welcome: string,
+    heading: string,
+    connectText: string,
+    contactBtn: string,
+  }, 
   footer: {
     madeWithText: string;
     inText: string;
